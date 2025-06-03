cca6f343c97d9a44261fbd2faae8de2fe39f0e9a
Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-06-03 22:00:48 +0600

    Number 2 Problem solve done

---
+++ src/Problem2.ts
@@ -0,0 +1,25 @@
+// Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+type Book = {
+  title: string;
+  author: string;
+  year: number;
+};
+
+const books: Book[] = [
+  { title: "Nakshi Kanthar Math", author: "Jasim Uddin", year: 1929 },
+  { title: "Padma Nadir Majhi", author: "Manik Bandopadhyay", year: 1936 },
+  {
+    title: "Lalsalu (Tree Without Roots)",
+    author: "Syed Waliullah",
+    year: 1948,
+  },
+  { title: "Ekattorer Dinguli", author: "Jahanara Imam", year: 1986 },
+  { title: "Shesher Kobita", author: "Rabindranath Tagore", year: 1929 },
+];
+
+const getBookTitles = (arr: Book[]): string[] => {
+  return arr.map((book) => book.title);
+};
+
+console.log(getBookTitles(books));
